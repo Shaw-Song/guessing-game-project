4554d734d8a030983b035e43a7e3c556c484304d
Shaw <shawsong2022@gmail.com>
2023-01-29 11:30:05 -0800

    minimal viable

--- guessing-game.js
+++ guessing-game.js
@@ -1,4 +1,13 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
 let secretNumber = 4;
+
 function checkGuess(n) {
     if (n > secretNumber) {
         console.log("Too high");
@@ -6,10 +15,27 @@ function checkGuess(n) {
     } else if (n < secretNumber) {
         console.log("Too low");
         return false;
-    } else {
+    } else if (n === secretNumber) {
         return true;
     }
 }
+
+
+let askGuess = () => {
+    rl.question("Enter a guess: ", answer => {
+        if (checkGuess(Number(answer))) {
+            console.log("You win!");
+            rl.close();
+        } else {
+            askGuess();
+        }
+    });
+}
+
+
+askGuess();
+
+
 
 // console.log(checkGuess(1));
 // console.log(checkGuess(5));
